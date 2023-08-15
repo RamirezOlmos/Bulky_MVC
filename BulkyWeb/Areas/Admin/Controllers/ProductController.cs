6bc359f24032f2b253f91306c06aaba323577563
RamirezOlmos <nyy_715074@yahoo.com>
2023-08-14 21:18:31 -0600

    change the Product controller create to upsert to include create and edit in the same component

--- BulkyWeb/Areas/Admin/Controllers/ProductController.cs
+++ BulkyWeb/Areas/Admin/Controllers/ProductController.cs
@@ -24,7 +24,7 @@ namespace BulkyWeb.Areas.Admin.Controllers
 			return View(objProductList);
 		}
 
-		public IActionResult Create()
+		public IActionResult Upsert(int? id)
 		{
 			ProductVM productVM = new()
 			{
@@ -37,11 +37,21 @@ namespace BulkyWeb.Areas.Admin.Controllers
 				Product = new Product()
 			};
 
-			return View(productVM);
+			if (id == null || id == 0)
+			{
+				//create
+				return View(productVM);
+			}
+			else
+			{
+				//update
+				productVM.Product = _unitOfWork.Product.Get(u => u.Id == id);
+				return View(productVM);
+			}
 		}
 
 		[HttpPost]
-		public IActionResult Create(ProductVM productVM)
+		public IActionResult Upsert(ProductVM productVM, IFormFile? file)
 		{
 			//if (newProduct.Name == newProduct.DisplayOrder.ToString())
 			//{
